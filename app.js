3d4b9df97ee8a6bc3f55adb58235db3bc83048c2
samdd1 <109146974+samdd1@users.noreply.github.com>
2023-04-15 22:01:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post("/",function(req,res){
     const url = "https://us13.api.mailchimp.com/3.0/lists/164cb05b05";
     const options = {
         method:"POST",
-        auth:"m:2c1bbf61a3282122da71f6a486e2564d-us13"
+        auth:"m:f7c48888c9fa208301683225ff3ad47f-us13"
     }
    const request =  https.request(url,options,function(response){
             if(response.statusCode == 200){
